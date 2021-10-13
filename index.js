53d05ec1d24f37b1182445b6af8606e44bdba41a
MinhHoang <hoangle1413@gmail.com>
2021-10-13 07:42:02 +0700

    frontend

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(express.json())
 app.use(cors())
+app.use(express.static("build"))
 let persons = [
     { 
       "id": 1,
